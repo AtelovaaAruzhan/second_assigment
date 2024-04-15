35ed011194dd1b1c77212e773d00e3be40522526
aruzhan <atelova_aruzhann@mail.ru>
2024-04-15 09:46:09 +0500

    classwork

--- src/hello.java
+++ src/hello.java
@@ -1,7 +1,22 @@
-public class hello {
-    System.out.println("Hello")
+public main (){
+    public class ChangeToUpperCase {
 
+        public static String toUpperCaseRecursive(String str) {
+            if (str.isEmpty()) {
+                return "";
+            }
 
+            char firstChar = str.charAt(0);
+            String remaining = str.substring(1);
 
+            return Character.isUpperCase(firstChar) ? firstChar + remaining : Character.toUpperCase(firstChar) + toUpperCaseRecursive(remaining);
+        }
+
+        public static void main(String[] args) {
+            String str = "hello, world!";
+            System.out.println("Строка в нижнем регистре: " + str);
+            System.out.println("Строка в верхнем регистре: " + toUpperCaseRecursive(str));
+        }
+    }
 
 }
